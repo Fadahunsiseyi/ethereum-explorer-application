6ac7e9eb91e877e3b6ef79164991f478a8429be6
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-30 09:36:55 +0100

    Setup linters for the application

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
 
 function App() {
-  const url = 'https://api.etherscan.io/api?module=account&action=txlist&address=0xc5102fE9359FD9a28f877a67E36B0F050d81a3CC&startblock=0&endblock=99999999&page=1&offset=30&sort=asc&apikey=G7A3WNIB4ATIDHIJIVTYU76DY6PVYQYECC'
-const data = fetch(url).then((res) => res.json().then((json) => {
- const {result} = json;
- console.log(result);
-}))
- console.log(data,'the data');
   return (
     <div className="App">
-     <h1>Hello</h1>
+      <h1>Hello</h1>
     </div>
   );
 }
