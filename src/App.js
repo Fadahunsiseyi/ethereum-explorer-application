d06e4a2570e460557d76e45296da112ab9ee3158
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-30 09:23:31 +0100

    Remove CRA boilerplates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const url = 'https://api.etherscan.io/api?module=account&action=txlist&address=0xc5102fE9359FD9a28f877a67E36B0F050d81a3CC&startblock=0&endblock=99999999&page=1&offset=30&sort=asc&apikey=G7A3WNIB4ATIDHIJIVTYU76DY6PVYQYECC'
+const data = fetch(url).then((res) => res.json().then((json) => {
+ const {result} = json;
+ console.log(result);
+}))
+ console.log(data,'the data');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hello</h1>
     </div>
   );
 }
