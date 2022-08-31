7de1151c411dac4ee33f0ef4638d71ff1a4c3d2e
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-31 14:48:56 +0100

    Dispatch the action on page load with useEffect, and passed the order

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
-import GetApi from './api/Api';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import NotFound from './pages/NotFound';
 import TransactionPage from './pages/TransactionPage';
-
-GetApi();
+import { displayTransaction } from './redux/transaction/TransactionReducer';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(displayTransaction({ order: 'asc' }));
+  }, []);
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/transaction" element={<TransactionPage />} />
+        <Route path="/" element={<HomePage />} exact />
+        <Route path="/transactions/:id" element={<TransactionPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
--- src/api/Api.js
+++ src/api/Api.js
@@ -3,15 +3,16 @@ const URL_ADDRESS = '0xc5102fE9359FD9a28f877a67E36B0F050d81a3CC';
 const API_KEY = 'G7A3WNIB4ATIDHIJIVTYU76DY6PVYQYECC';
 // const API_SORT = 'asc';
 
-export default function GetApi() {
-  const url = `${BASE_URL}?module=account&action=txlist&address=${URL_ADDRESS}&startblock=0&endblock=99999999&page=1&offset=30&sort=asc&apikey=${API_KEY}`;
+export default function GetApi({ order }) {
+  console.log(order, 'I am the order');
+  const url = `${BASE_URL}?module=account&action=txlist&address=${URL_ADDRESS}&startblock=0&endblock=99999999&page=1&offset=30&sort=${order}&apikey=${API_KEY}`;
   const getData = fetch(url)
     .then((res) => res.json().then((res) => {
-      const { result, status } = res;
-      console.log(result, status);
-      if (status === 1) return result;
-      throw Error('Invalid transactions');
+      const { result } = res;
+      return result;
     }))
-    .catch((err) => err);
+    .catch((err) => {
+      throw Error(err);
+    });
   return getData;
 }
