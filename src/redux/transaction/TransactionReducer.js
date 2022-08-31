40b3aa3328b4a59cfe6cf750a84e314df20f7810
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-31 10:41:10 +0100

    Add the search reducer, and combined the reducers

--- src/redux/transaction/TransactionReducer.js
+++ src/redux/transaction/TransactionReducer.js
@@ -17,3 +17,33 @@ export const failedTransactions = (result) => ({
   type: TRANSACTION_FAIL,
   payload: result,
 });
+
+const initial = {
+  transactions: [],
+  loading: true,
+  error: false,
+};
+
+export default function TransactionReducer(state = initial, action) {
+  switch (action.type) {
+    case TRANSACTION_FETCHING:
+      return { ...state, loading: true, error: null };
+    case TRANSACTION_FAIL:
+      return { ...state, loading: false, error: true };
+    case TRANSACTION_SUCCESS:
+      return { transactions: action.payload, loading: false, error: null };
+    default:
+      return state;
+  }
+}
+
+export const displayTransaction = () => async (dispatch) => {
+  dispatch(fetchingTransactions());
+  try {
+    const data = await GetApi();
+    dispatch({ type: TRANSACTION_SUCCESS, payload: data });
+    return data;
+  } catch (error) {
+    return dispatch(failedTransactions(error));
+  }
+};
