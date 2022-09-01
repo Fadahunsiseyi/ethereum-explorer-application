743334d74531e21524664d06b541a4021207b61f
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-09-01 16:42:06 +0100

    Display the sum of the values in the transactions

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,9 +1,11 @@
 const BASE_URL = 'https://api.etherscan.io/api';
-const URL_ADDRESS = '0xc5102fE9359FD9a28f877a67E36B0F050d81a3CC';
+const URL_ADDRESS = '0x4e83362442b8d1bec281594cea3050c8eb01311c';
+const CONTRACT_ADDRESS = '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2';
+const END_BLOCK = '27025780';
 const API_KEY = 'G7A3WNIB4ATIDHIJIVTYU76DY6PVYQYECC';
 
 export default function GetApi({ order }) {
-  const url = `${BASE_URL}?module=account&action=txlist&address=${URL_ADDRESS}&startblock=0&endblock=99999999&page=1&offset=30&sort=${order}&apikey=${API_KEY}`;
+  const url = `${BASE_URL}?module=account&action=tokentx&contractaddress=${CONTRACT_ADDRESS}&address=${URL_ADDRESS}&page=1&offset=100&startblock=0&endblock=${END_BLOCK}&sort=${order}&apikey=${API_KEY}`;
   const getData = fetch(url)
     .then((res) => res.json().then((res) => {
       const { result } = res;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const HomePage = () => {
+  const selector = useSelector((state) => state);
+  const { TransactionReducer: transactions, SearchReducer: search } = selector;
+  const [total, setTotal] = useState({ title: '', subtitle: '' });
+  useEffect(() => {
+    if (transactions.transactions.length > 0) {
+      // eslint-disable-next-line max-len
+      const sumTransactions = transactions.transactions.reduce((a, b) => a + parseInt(b.value, 10), 0).toLocaleString();
+      setTotal({ title: 'Binance 14 Ethereum', subtitle: `ETHEREUM HAS A  TOTAL VALUE OF ${sumTransactions}` });
+    }
+  }, [transactions]);
   return (
     <div>
       HomePage
     </div>
-  )
-}
+  );
+};
 
 export default HomePage;
