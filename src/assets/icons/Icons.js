058ab45fe1bb8768076f475a3eab9c87c95eba66
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-09-03 01:52:15 +0100

    Fix linters error

--- src/assets/icons/Icons.js
+++ src/assets/icons/Icons.js
@@ -1,124 +1,29 @@
+import React from 'react';
+import { BiLeftArrowAlt, BiRightArrowAlt, BiMicrophone } from 'react-icons/bi';
+import { IoIosSettings } from 'react-icons/io';
+
 export function EthereumIcon() {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 256 417"
-      version="1.1"
-      preserveAspectRatio="xMidYMid"
-    >
-      <g>
-        <polygon
-          fill="#343434"
-          points="127.9611 0 125.1661 9.5 125.1661 285.168 127.9611 287.958 255.9231 212.32"
-        />
-        <polygon
-          fill="#8C8C8C"
-          points="127.962 0 0 212.32 127.962 287.959 127.962 154.158"
-        />
-        <polygon
-          fill="#3C3C3B"
-          points="127.9611 312.1866 126.3861 314.1066 126.3861 412.3056 127.9611 416.9066 255.9991 236.5866"
-        />
-        <polygon
-          fill="#8C8C8C"
-          points="127.962 416.9052 127.962 312.1852 0 236.5852"
-        />
-        <polygon
-          fill="#141414"
-          points="127.9611 287.9577 255.9211 212.3207 127.9611 154.1587"
-        />
-        <polygon
-          fill="#393939"
-          points="0.0009 212.3208 127.9609 287.9578 127.9609 154.1588"
-        />
-      </g>
-    </svg>
+    <img
+      src="https://cryptologos.cc/logos/ethereum-eth-logo.png"
+      alt="ethlogo"
+      style={{ width: '45%' }}
+    />
   );
 }
 
 export function ArrowCircleRightIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="circleRightIcon"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeWidth={2}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z"
-      />
-    </svg>
-  );
+  return <BiRightArrowAlt style={{ fontSize: '2rem', color: '#fff' }} />;
 }
 
 export function ChevronLeftIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="chevIcon"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeWidth={2}
-    >
-      <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
-    </svg>
-  );
-}
-
-export function SearchIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="searchIcon"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeWidth={2}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-      />
-    </svg>
-  );
+  return <BiLeftArrowAlt style={{ fontSize: '2rem', color: '#fff' }} />;
 }
 
 export function MicrophoneIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="micIcon"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-    >
-      <path
-        fillRule="evenodd"
-        d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
+  return <IoIosSettings style={{ fontSize: '2rem', color: '#fff' }} />;
 }
 
 export function CogIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="cogIcon"
-      viewBox="0 0 20 20"
-      fill="currentColor"
-    >
-      <path
-        fillRule="evenodd"
-        d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
+  return <BiMicrophone style={{ fontSize: '2rem', color: '#fff' }} />;
 }
